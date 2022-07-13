6d7885f13b2c8ecf2d55d0a2440485f05dd9b545
Luis <luis.leutbecher@tum.de>
2022-07-13 17:06:37 +0200

    push2

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,17 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.toLowerCase().contains("what is your name")) {
-            return "Luis 2";
+            return "Luis";
+        } else if (query.toLowerCase().contains("what is") && query.toLowerCase().contains("plus")) {
+            int first = query.indexOf("what is ") + 9;
+            int second = query.indexOf("plus ") + 5;
+
+            String x = query.substring(query.indexOf("what is "), first);
+            String y = query.substring(query.indexOf("plus "), second);
+
+
+            return String.valueOf(Integer.parseInt(x) + Integer.parseInt(y));
+
         } else { // TODO extend the programm here
             return "";
         }
